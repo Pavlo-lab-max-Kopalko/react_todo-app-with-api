80b10f62ca4c94176be8dc837dadf5f00a848a0f
Pavlo <oksanakopalko@gmail.com>
2025-05-19 22:54:49 +0300

    як зробити так щоб сторніка не перезавантажувалася після події сабміт

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,203 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { deleteTodos, getTodos, patchTodos, USER_ID } from './api/todos';
+import { ErrorMessage, FilteredStatus, Todo } from './types/Todo';
+import { TodoList } from './components/TodoList';
+import cn from 'classnames';
+import { FormAddTodo } from './components/FormAddTodo';
+import { Footer } from './components/Footer';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState<ErrorMessage>(
+    ErrorMessage.DEFAULT,
+  );
+  const [count, setCount] = useState<number>(0);
+  const [filterValue, setFilterValue] = useState<FilteredStatus>(
+    FilteredStatus.ALL,
+  );
+  const [tempTodo, setTempTodo] = useState<null | Todo>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [deletedTodoId, setDeletedTodoId] = useState<number[]>([]);
+  const [editingTodoId, setEditingTodoId] = useState<number | undefined>();
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => {
+        setErrorMessage(ErrorMessage.LOAD);
+        setTimeout(() => {
+          setErrorMessage(ErrorMessage.DEFAULT);
+        }, 3000);
+      });
+  }, []);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
-  return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-add-and-delete#react-todo-app-add-and-delete">
-          React Todo App - Add and Delete
-        </a>
-      </p>
+  const onHandler = () => {
+    setErrorMessage(ErrorMessage.DEFAULT);
+  };
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setErrorMessage(ErrorMessage.DEFAULT);
+  };
+
+  const onInputChange = (
+    todoId: number,
+    change: string,
+    value?: HTMLInputElement['value'],
+  ) => {
+    const seekTodo = todos.find(todo => todo.id === todoId);
+
+    if (change === 'status') {
+      const backStatus = seekTodo?.completed;
+      const changArg = { completed: !backStatus };
+
+      console.log(backStatus);
+
+      patchTodos(todoId, changArg)
+        .then(response => {
+          const mappedTodos: Todo[] = todos.map(todo => {
+            console.log(response);
+
+            if (todo.id === todoId) {
+              return response as Todo;
+            }
+
+            return todo;
+          });
+
+          setTodos(mappedTodos);
+        })
+        .catch(() => {
+          setErrorMessage(ErrorMessage.UPDATE);
+        });
+    }
+
+    if (change === 'title') {
+      const changArg = { title: value };
+
+      // if (changArg.title === '') {
+      //   deleteTodos(todoId)
+      //     .then(() => {
+      //       inputRef.current?.focus();
+      //       const exsistedTodos = todos.filter(todo => todo.id !== todoId);
+
+      //       setTodos(exsistedTodos);
+      //     })
+      //     .catch(() => {
+      //       setErrorMessage(ErrorMessage.DELETE);
+
+      //       setTimeout(() => {
+      //         setErrorMessage(ErrorMessage.DEFAULT);
+      //       }, 3000);
+      //     })
+      //     .finally(() =>
+      //       setDeletedTodoId(prevIds => prevIds.filter(id => id !== todoId)),
+      //     );
+      // }
+
+      patchTodos(todoId, changArg)
+        .then(response => {
+          const mappedTodos: Todo[] = todos.map(todo => {
+            console.log(response);
+
+            if (todo.id === todoId) {
+              return response as Todo;
+            }
+
+            return todo;
+          });
+
+          setTodos(mappedTodos);
+        })
+        .catch(() => {
+          setErrorMessage(ErrorMessage.UPDATE);
+        });
+    }
+  };
+
+  console.log(todos);
+
+  return (
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <header className="todoapp__header">
+          {/* this button should have `active` class only if all todos are completed */}
+          <button
+            type="button"
+            className="todoapp__toggle-all active"
+            data-cy="ToggleAllButton"
+          />
+
+          {/* Add a todo on form submit */}
+          <FormAddTodo
+            onSubmit={handleSubmit}
+            setTodos={setTodos}
+            todos={todos}
+            setErrorMessage={setErrorMessage}
+            setTempTodo={setTempTodo}
+            tempTodo={tempTodo}
+            inputRef={inputRef}
+          />
+        </header>
+
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          setCount={setCount}
+          filterValue={filterValue}
+          onInputChange={onInputChange}
+          tempTodo={tempTodo}
+          setErrorMessage={setErrorMessage}
+          inputRef={inputRef}
+          deletedTodoId={deletedTodoId}
+          setDeletedTodoId={setDeletedTodoId}
+          editingTodoId={editingTodoId}
+          setEditingTodoId={setEditingTodoId}
+        />
+
+        {/* Hide the footer if there are no todos */}
+
+        {todos.length > 0 && (
+          <Footer
+            count={count}
+            filterValue={filterValue}
+            setFilterValue={setFilterValue}
+            todos={todos}
+            setTodos={setTodos}
+            inputRef={inputRef}
+            setErrorMessage={setErrorMessage}
+          />
+        )}
+      </div>
+
+      {/* DON'T use conditional rendering to hide the notification */}
+      {/* Add the 'hidden' class to hide the message smoothly */}
+      <div
+        data-cy="ErrorNotification"
+        className={cn(
+          'notification is-danger is-light has-text-weight-normal',
+          { hidden: !errorMessage },
+        )}
+      >
+        <button
+          data-cy="HideErrorButton"
+          type="button"
+          className="delete"
+          onClick={onHandler}
+        />
+        {/* show only one message at a time */}
+        {errorMessage}
+      </div>
+    </div>
   );
 };
---
+++ src/components/TodoItem.tsx/TodoItem.tsx
@@ -0,0 +1,120 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { useState } from 'react';
+import { Todo } from '../../types/Todo';
+import cn from 'classnames';
+import { deleteTodos } from '../../api/todos';
+import { event } from 'cypress/types/jquery';
+
+interface Props {
+  todo: Todo;
+  onInputChange: (
+    todoId: number,
+    change: string,
+    value?: HTMLInputElement['value'],
+  ) => void;
+  onDelete: (todoId: number) => void;
+  deletedTodoId: number[];
+  editingTodoId: number | undefined;
+  setEditingTodoId: (value: number) => void;
+  inputRef: React.MutableRefObject<HTMLInputElement | null>;
+}
+
+export const TodoItem = ({
+  todo,
+  onInputChange,
+  onDelete,
+  deletedTodoId,
+  editingTodoId,
+  setEditingTodoId,
+  inputRef,
+}: Props) => {
+  const [titleTodo, setTitleTodo] = useState<string>(todo.title);
+  const handleBlur = (
+    value: HTMLInputElement['value'],
+    e: React.FocusEvent<HTMLInputElement, Element>,
+  ) => {
+    if (value === '') {
+      // deleteTodos(todo.id)
+      //   .then(() => {
+      //     inputRef.current?.focus();
+      //     const exsistedTodos = todos.filter(todo => todo.id !== todoId);
+
+      //     setTodos(exsistedTodos);
+      //   })
+      //   .catch(() => {
+      //     setErrorMessage(ErrorMessage.DELETE);
+
+      //     setTimeout(() => {
+      //       setErrorMessage(ErrorMessage.DEFAULT);
+      //     }, 3000);
+      //   })
+      //   .finally(() =>
+      //     setDeletedTodoId(prevIds => prevIds.filter(id => id !== todoId)),
+      //   );
+    }
+
+    e.preventDefault();
+  };
+
+  return (
+    <div
+      key={todo.id}
+      data-cy="Todo"
+      className={cn('todo item-enter-done', { completed: todo.completed })}
+    >
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+          onChange={() => onInputChange(todo.id, 'status')}
+        />
+      </label>
+
+      {editingTodoId !== todo.id ? (
+        <span
+          data-cy="TodoTitle"
+          className="todo__title"
+          onDoubleClick={() => setEditingTodoId(todo.id)}
+        >
+          {todo.title}
+        </span>
+      ) : (
+        <form>
+          <input
+            data-cy="TodoTitleField"
+            type="text"
+            className="todo__title-field"
+            placeholder="Empty todo will be deleted"
+            value={titleTodo}
+            onChange={e => {
+              onInputChange(todo.id, 'title', e.target.value);
+              setTitleTodo(e.target.value);
+            }}
+            onBlur={e => handleBlur(e.target.value, e)}
+          />
+        </form>
+      )}
+
+      <button
+        type="button"
+        className="todo__remove"
+        data-cy="TodoDelete"
+        onClick={() => onDelete(todo.id)}
+      >
+        ×
+      </button>
+
+      <div
+        data-cy="TodoLoader"
+        className={cn('modal overlay', {
+          'is-active': deletedTodoId.includes(todo.id),
+        })}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
